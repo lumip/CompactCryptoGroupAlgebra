bf3955e2bf864f0a9f4f350bc19e0f27121bc641
Lukas Prediger <lukas.prediger@rwth-aachen.de>
2019-08-19 19:20:50 +0300

    diverse progress
    - Invert becomes Negate
    - Removing Scalar generic; now always BigInteger
    - Implemented MultiplyScalar in CryptoGroupAlgebra
    - Added FactorSize property to CryptoGroup and CryptoGroupAlgebra
    - Attempt at ECGroupAlgebra

--- CompactEC/CryptoAlgebra/CryptoGroup.cs
+++ CompactEC/CryptoAlgebra/CryptoGroup.cs
@@ -1,38 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Numerics;
 
 namespace CompactEC.CryptoAlgebra
 {
-    public abstract class CryptoGroup<E, S>
+    public abstract class CryptoGroup<E>
     {
-        private CryptoGroupAlgebra<E, S> _groupAlgebra;
+        private CryptoGroupAlgebra<E> _groupAlgebra;
 
-        public CryptoGroup(CryptoGroupAlgebra<E, S> groupAlgebra)
+        public CryptoGroup(CryptoGroupAlgebra<E> groupAlgebra)
         {
             if (groupAlgebra == null)
                 throw new ArgumentNullException(nameof(groupAlgebra));
             _groupAlgebra = groupAlgebra;
         }
 
-        public S Order { get { return _groupAlgebra.Order; } }
+        public BigInteger Order { get { return _groupAlgebra.Order; } }
 
         public int GroupElementSize { get { return _groupAlgebra.GroupElementSize; } }
         public int OrderSize { get { return _groupAlgebra.OrderSize; } }
+        public int FactorSize { get { return _groupAlgebra.FactorSize; } }
 
-        public CryptoGroupElement<E, S> Generator { get { return CreateGroupElement(_groupAlgebra.Generator, _groupAlgebra); } }
+        public CryptoGroupElement<E> Generator { get { return CreateGroupElement(_groupAlgebra.Generator, _groupAlgebra); } }
+        public CryptoGroupElement<E> IdentityElement { get { return CreateGroupElement(_groupAlgebra.IdentityElement, _groupAlgebra); } }
 
-        public CryptoGroupElement<E, S> GenerateElement(S index)
+        public CryptoGroupElement<E> GenerateElement(BigInteger index)
         {
             return CreateGroupElement(_groupAlgebra.GenerateElement(index), _groupAlgebra);
         }
 
-        public CryptoGroupElement<E, S> CreateElementFromBytes(byte[] buffer)
+        public CryptoGroupElement<E> CreateElementFromBytes(byte[] buffer)
         {
             return CreateGroupElement(buffer, _groupAlgebra);
         }
 
-        protected abstract CryptoGroupElement<E, S> CreateGroupElement(E e, CryptoGroupAlgebra<E, S> groupAlgebra);
-        protected abstract CryptoGroupElement<E, S> CreateGroupElement(byte[] buffer, CryptoGroupAlgebra<E, S> groupAlgebra);
+        protected abstract CryptoGroupElement<E> CreateGroupElement(E e, CryptoGroupAlgebra<E> groupAlgebra);
+        protected abstract CryptoGroupElement<E> CreateGroupElement(byte[] buffer, CryptoGroupAlgebra<E> groupAlgebra);
     }
 }
