3de9abd0b99e76d13faac9bbbfaf4d61aafa8e9b
Lukas Prediger <lumip@lumip.de>
2019-08-23 21:17:18 +0300

    Tests for CryptoGroupAlgebra and some for CryptoGroup.

---
+++ CompactEC/CryptoAlgebra/ICryptoGroupAlgebra.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace CompactEC.CryptoAlgebra
+{
+    public interface ICryptoGroupAlgebra<E> where E : struct
+    {
+        BigInteger Order { get; }
+        E Generator { get; }
+        int GroupElementBitlength { get; }
+        int OrderBitlength { get; }
+
+        E GenerateElement(BigInteger index);
+        E Negate(E e);
+        E MultiplyScalar(E e, BigInteger k);
+        E Add(E left, E right);
+        bool IsValid(E element);
+    }
+}
